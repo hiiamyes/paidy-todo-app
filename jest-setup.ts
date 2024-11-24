e6a772734ec146c5ce892d61ba6f359355f86505
Yes Lee <joshuayes@gmail.com>
2024-11-24 23:24:49 +0800

    Install and setup testing related packages and files

---
+++ jest-setup.ts
@@ -0,0 +1,6 @@
+import '@testing-library/react-native/extend-expect';
+
+// https://react-native-async-storage.github.io/async-storage/docs/advanced/jest/
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
+);
